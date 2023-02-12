5768697e124006a1d8aa68b653157bba2c4d9ae9
shashankmangal10@gmail.com <Sunil@1234>
2023-02-12 19:31:44 +0530

    can be uploaded 3

---
+++ app/src/main/java/com/sharkBytesLab/shortNotes/SplashActivity.kt
@@ -0,0 +1,51 @@
+package com.sharkBytesLab.shortNotes
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setStatusBarColor(Color.parseColor("#ffffff"))
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+        }, 2000)
+
+
+    }
+
+    fun Activity.setStatusBarColor(color: Int) {
+        var flags = window?.decorView?.systemUiVisibility // get current flag
+        if (flags != null) {
+            if (isColorDark(color)) {
+                flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                window?.decorView?.systemUiVisibility = flags
+            } else {
+                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                window?.decorView?.systemUiVisibility = flags
+            }
+        }
+        window?.statusBarColor = color
+    }
+
+    fun Activity.isColorDark(color: Int): Boolean {
+        val darkness =
+            1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+        return darkness >= 0.5
+    }
+
+}
